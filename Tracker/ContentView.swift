b7cff505c05c4914aebbe2b4d6da4b4dfb34bf7f
Nadezhda Chelyadinova <nadzyathedude@yandex.ru>
2026-04-17 13:59:59 +0600

    Initial Commit

---
+++ Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tracker
+//
+//  Created by nadzya on 17.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by nadzya on 17.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
